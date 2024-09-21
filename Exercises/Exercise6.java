273b27dfd7c11a1064a9ccd7cad94df23acedd3a
felixdev <135524882+felixmk0@users.noreply.github.com>
2024-09-21 21:18:37 +0200

    Actualizar Exercises

---
+++ Exercises/Exercise6.java
@@ -0,0 +1,22 @@
+package Exercises;
+
+/*
+    Dibuja un cuadrado con relleno (*), que nos pida su tamaño por entrada a través de la clase Scanner de java.util
+*/
+
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce el número de elementos (*) del cuadrado:");
+        int e = scanner.nextInt();
+
+        // f = filas, e = elementos (*) y c = columnas
+        for (int f = 1; f <= e; f++) {
+            for (int c = 1; c <= e; c++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
